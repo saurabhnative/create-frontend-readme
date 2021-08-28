521ef5895e0c55c998cb0e782564d097ee72dab9
Saurabh Mhatre <codeclassifiers@gmail.com>
2021-08-28 09:55:55 +0530

    Video changes added

--- components/popupComponent/IntroVideoPopUp.js
+++ components/popupComponent/IntroVideoPopUp.js
@@ -1,6 +1,6 @@
-import { Fragment, useRef, useState } from "react";
+import { Fragment, useRef } from "react";
 import { Dialog, Transition } from "@headlessui/react";
-
+import { IoCloseCircleSharp } from "react-icons/io5";
 export default function IntroVideoPopUp(props) {
   const cancelButtonRef = useRef(null);
 
@@ -12,7 +12,7 @@ export default function IntroVideoPopUp(props) {
         initialFocus={cancelButtonRef}
         onClose={props.setOpen}
       >
-        <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+        <div className="flex items-center justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0 relative">
           <Transition.Child
             as={Fragment}
             enter="ease-out duration-300"
@@ -32,6 +32,7 @@ export default function IntroVideoPopUp(props) {
           >
             &#8203;
           </span>
+
           <Transition.Child
             as={Fragment}
             enter="ease-out duration-300"
@@ -41,21 +42,24 @@ export default function IntroVideoPopUp(props) {
             leaveFrom="opacity-100 translate-y-0 sm:scale-100"
             leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
           >
-            <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
-              <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-                <div className="sm:flex sm:items-start">
-                  <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left"></div>
-                </div>
+            <div className="intro-video-popup relative inline-block align-bottom bg-white rounded-lg text-left shadow-xl transform transition-all sm:my-8 sm:align-middle">
+              <div
+                className="absolute -right-2 -top-2 text-3xl z-10 cursor-pointer text-red-600"
+                onClick={() => props.setOpen(false)}
+              >
+                <IoCloseCircleSharp />
               </div>
-              <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
-                <button
-                  type="button"
-                  className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
-                  onClick={() => props.setOpen(false)}
-                  ref={cancelButtonRef}
-                >
-                  Close
-                </button>
+              <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4 h-full">
+                <div className="h-full">
+                  <iframe
+                    className="w-full h-full"
+                    src="https://www.youtube.com/embed/An6bmCHy7Q0"
+                    title="YouTube video player"
+                    frameBorder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen
+                  ></iframe>
+                </div>
               </div>
             </div>
           </Transition.Child>
