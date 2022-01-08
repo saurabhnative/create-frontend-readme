a622df360df925a5a3d9ee6c10506924cf7dcda3
Saurabh Mhatre <codeclassifiers@gmail.com>
2022-01-08 12:39:43 +0530

    Webpack Changes

--- next.config.js
+++ next.config.js
@@ -3,10 +3,14 @@
 // https://nextjs.org/docs/api-reference/next.config.js/introduction
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-const { withSentryConfig } = require('@sentry/nextjs');
+const { withSentryConfig } = require("@sentry/nextjs");
 
 const moduleExports = {
   // Your existing module.exports
+  sentry: {
+    disableServerWebpackPlugin: true,
+    disableClientWebpackPlugin: true,
+  },
 };
 
 const sentryWebpackPluginOptions = {
